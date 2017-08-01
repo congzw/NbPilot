68979c950ecc1893136b52a62ffc07e6aba5c35b
congzw <46074987@qq.com>
2017-08-01 10:44:07 +0800

    add Test project for branch demo

---
+++ src/NbCloud.Common.Test/Demos/DemoSpecs.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NbCloud.Common.Test.Demos
+{
+    [TestClass]
+    public class DemoSpecs
+    {
+        [TestMethod]
+        public void Demo_Test_Case()
+        {
+        }
+    }
+}
