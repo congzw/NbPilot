e155e6dd16b48d2e09dcf809506aef7599e604c6
congzw <46074987@qq.com>
2017-08-01 14:12:25 +0800

    add INbObject(弱类型约束接口)

---
+++ src/NbCloud.Common/NbObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NbCloud.Common
+{
+    /// <summary>
+    /// 弱类型约束接口
+    /// </summary>
+    public interface INbObject
+    {
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class INbObjectExtensions
+    {
+        /// <summary>
+        /// 转换弱类型约束接口为具体类型
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="nbObject"></param>
+        /// <returns></returns>
+        public static T As<T>(this INbObject nbObject) where T : class, INbObject
+        {
+            return nbObject as T;
+        }
+    }
+}
---
+++ src/NbCloud.Common/_Specs/NbObjectSpecs.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NbCloud.Common._Specs
+{
+    [TestClass]
+    public class NbObjectSpecs
+    {
+        [TestMethod]
+        public void As_Null_Should_Return_Null()
+        {
+            INbObject myDemo = null;
+            var demoNbObject = myDemo.As<_DemoNbObject>();
+            Assert.IsNull(demoNbObject);
+        }
+
+        [TestMethod]
+        public void As_Instance_Should_Return_Not_Null()
+        {
+            INbObject myDemo = new _DemoNbObject();
+            var demoNbObject = myDemo.As<_DemoNbObject>();
+            Assert.IsNotNull(demoNbObject);
+        }
+    }
+    
+    #region demo
+
+    internal class _DemoNbObject : INbObject
+    {
+        public string Name { get; set; }
+    }
+
+    internal class _DemoNbObject2
+    {
+        
+    }
+
+    #endregion
+}
