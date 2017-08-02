feb5bc6d71c63364b29c8be7c1f8b437d732b468
congzw <46074987@qq.com>
2017-08-02 18:09:22 +0800

    add IO Extensions

---
+++ src/NbCloud.Common/IO/Extensions/StreamExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace NbCloud.Common.IO.Extensions
+{
+    /// <summary>
+    /// StreamExtensions
+    /// </summary>
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// 获取所有字节
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static byte[] GetAllBytes(this Stream stream)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
