623586974e6c19609d5b57e5238190f03e216a4f
congzw <46074987@qq.com>
2017-08-02 13:56:43 +0800

    add test extensions

---
+++ src/NbCloud.Common.Test/Collections/TypeListSpecs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NbCloud.Common.Collections
+{
+    [TestClass]
+    public class TypeListSpecs
+    {
+        [TestMethod]
+        public void Should_Only_Add_True_Types()
+        {
+            var list = new TypeList<IMyInterface>();
+            list.Add<MyClass1>();
+            list.Add(typeof(MyClass2));
+            Action action = () => list.Add(typeof(MyClass3));
+            action.ShouldThrows<ArgumentException>();
+            AssertHelper.ShouldThrows<ArgumentException>(() => list.Add(typeof(MyClass3)));
+        }
+
+        public interface IMyInterface
+        {
+
+        }
+
+        public class MyClass1 : IMyInterface
+        {
+
+        }
+
+        public class MyClass2 : IMyInterface
+        {
+
+        }
+
+        public class MyClass3
+        {
+
+        }
+    }
+}
